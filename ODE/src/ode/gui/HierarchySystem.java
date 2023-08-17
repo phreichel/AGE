b111889956a0c656c31cf0efe50eafdef23b405a
pre7618 <pre7618@USBNB56.philotech.corp>
2023-08-17 17:43:37 +0200

    GUI as ECS

---
+++ ODE/src/ode/gui/HierarchySystem.java
@@ -0,0 +1,79 @@
+//*************************************************************************************************
+package ode.gui;
+//*************************************************************************************************
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import ode.event.Event;
+import ode.event.Events;
+
+//*************************************************************************************************
+public class HierarchySystem {
+
+	//=============================================================================================
+	private Map<Widget, Hierarchy> hierarchyMap;
+	//=============================================================================================
+	
+	//=============================================================================================
+	public void registerEventHandlers(Events events) {
+		events.register(Event.NONE, this::handleAddWidget);
+		events.register(Event.NONE, this::handleDeleteWidget);
+		events.register(Event.NONE, this::handleMoveWidget);
+	}
+	//=============================================================================================
+	
+	//=============================================================================================
+	private void handleAddWidget(Event e) {
+		Widget widget = (Widget) e.data[0];
+		Widget parent = (Widget) e.data[1];
+		Hierarchy widgetHierarchy = hierarchyMap.get(widget);
+		Hierarchy parentHierarchy = hierarchyMap.get(parent);
+		widgetHierarchy.parent = parent;
+		Widget child = parentHierarchy.first;
+		if (child == null) {
+			parentHierarchy.first = widget;
+			widgetHierarchy.prev = null;
+			widgetHierarchy.next = null;
+		} else {
+			Hierarchy childHierarchy = hierarchyMap.get(child);
+			while (childHierarchy.next != null) {
+				child = childHierarchy.next;
+				childHierarchy = hierarchyMap.get(child);
+			}
+			childHierarchy.next = widget;
+			widgetHierarchy.prev = child;
+			widgetHierarchy.next = null;
+		}
+	}
+	//=============================================================================================
+
+	//=============================================================================================
+	private void handleDeleteWidget(Event e) {
+		Widget widget = (Widget) e.data[0];
+	}
+	//=============================================================================================
+
+	//=============================================================================================
+	private void handleMoveWidget(Event e) {
+		Widget widget = (Widget) e.data[0];
+		Widget parent = (Widget) e.data[1];
+	}
+	//=============================================================================================
+	
+	//=============================================================================================
+	public void update() {
+		for (Entry<Widget, Hierarchy> entry : hierarchyMap.entrySet()) {
+			update(entry.getKey(), entry.getValue());
+		}
+	}
+	//=============================================================================================
+
+	//=============================================================================================
+	private void update(Widget w, Hierarchy h) {
+		
+	}
+	//=============================================================================================
+	
+}
+//*************************************************************************************************
