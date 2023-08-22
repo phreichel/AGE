3d5f9b6c1c6bc7442eb6054b9c63ef5ee7d06a1d
Philip Reichel <phreichel@gmx.de>
2023-08-22 06:41:16 +0200

    ...

--- ODE/src/ode/gui/GUI.java
+++ ODE/src/ode/gui/GUI.java
@@ -61,7 +61,7 @@ public class GUI {
 	//=============================================================================================
 	public Widget createButton(String label, Action action) {
 		Widget widget = createWidget();
-		widget.setBounds(10, 10, 80, 20);
+		widget.setBounds(0, 0, 120, 20);
 		RenderData renderData = new RenderData();
 		renderData.type = RenderData.BUTTON;
 		renderMap.put(widget, renderData);
